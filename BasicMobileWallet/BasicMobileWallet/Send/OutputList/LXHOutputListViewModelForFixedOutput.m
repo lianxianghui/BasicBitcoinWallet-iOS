1dcef7b0396981829f54e1cc27ff1aeecfc9b9f4
Lian <lianxianghui@hotmail.com>
2019-12-04 16:38:36 +0800

    输出约束部分实现

---
+++ BasicMobileWallet/BasicMobileWallet/Send/OutputList/LXHOutputListViewModelForFixedOutput.m
@@ -0,0 +1,13 @@
+//
+//  LXHOutputListViewModelForFixedOutput.m
+//  BasicMobileWallet
+//
+//  Created by lian on 2019/12/4.
+//  Copyright © 2019年 lianxianghui. All rights reserved.
+//
+
+#import "LXHOutputListViewModelForFixedOutput.h"
+
+@implementation LXHOutputListViewModelForFixedOutput
+
+@end
