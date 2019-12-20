66739bd597572986498fea216cd11f42dcb443e7
Lian <lianxianghui@hotmail.com>
2019-12-20 15:34:14 +0800

    拉下的

--- BasicMobileWallet/BasicMobileWallet/Init/WalletMnemonicPassphrase/LXHWalletMnemonicPassphraseViewModel.h
+++ BasicMobileWallet/BasicMobileWallet/Init/WalletMnemonicPassphrase/LXHWalletMnemonicPassphraseViewModel.h
@@ -18,7 +18,6 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)initWithWords:(NSArray *)words;
 - (NSString *)viewClassName;
 - (id)setPassphraseViewModel;
-
 - (LXHWalletGenerationType)walletGenerationType;
 @end
 
