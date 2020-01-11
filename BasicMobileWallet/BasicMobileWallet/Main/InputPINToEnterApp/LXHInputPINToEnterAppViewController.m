2d6ea97cc1625e0c82959201700c52317487e32e
Lian <lianxianghui@hotmail.com>
2020-01-11 14:16:59 +0800

    部分实现

--- BasicMobileWallet/BasicMobileWallet/Main/InputPINToEnterApp/LXHInputPINToEnterAppViewController.m
+++ BasicMobileWallet/BasicMobileWallet/Main/InputPINToEnterApp/LXHInputPINToEnterAppViewController.m
@@ -13,8 +13,10 @@
         green:((rgbaValue & 0x00FF0000) >>  16)/255.0 \
         blue:((rgbaValue & 0x0000FF00) >>  8)/255.0 \
         alpha:(rgbaValue & 0x000000FF)/255.0]
+
+#define PINLength 6
     
-@interface LXHInputPINToEnterAppViewController()
+@interface LXHInputPINToEnterAppViewController() <UITextFieldDelegate>
 @property (nonatomic) LXHInputPINToEnterAppView *contentView;
 
 @end
@@ -35,6 +37,7 @@
     }];
     UISwipeGestureRecognizer *swipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeView:)];
     [self.view addGestureRecognizer:swipeRecognizer];
+    [self setViewProperties];
     [self addActions];
     [self setDelegates];
 }
@@ -44,9 +47,31 @@
 }
 
 - (void)addActions {
+    [self.contentView.inputPinTextField addTarget:self action:@selector(textFieldDidChanged:) forControlEvents:UIControlEventEditingChanged];
+}
+
+- (void)textFieldDidChanged:(UITextField *)textField {
+    if (textField.text.length == PINLength) {
+        
+    }
 }
 
 - (void)setDelegates {
+    self.contentView.inputPinTextField.delegate = self;
+}
+
+- (void)setViewProperties {
+    self.contentView.inputPinTextField.keyboardType = UIKeyboardTypeNumberPad;
+}
+
+//Delegate methods
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    // Prevent crashing undo bug
+    if (range.length + range.location > textField.text.length) {
+        return NO;
+    }
+    NSUInteger newLength = [textField.text length] + [string length] - range.length;
+    return newLength <= PINLength;
 }
 
 @end
