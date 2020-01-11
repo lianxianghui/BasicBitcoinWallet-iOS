94e50f923f7fa847dd388630293318935ed87d2d
Lian <lianxianghui@hotmail.com>
2020-01-11 16:56:35 +0800

    注释

--- BasicMobileWallet/BasicMobileWallet/AppDelegate.m
+++ BasicMobileWallet/BasicMobileWallet/AppDelegate.m
@@ -31,6 +31,9 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    //为了隐私
+    //1.进入后台时，ios系统会截屏，这里把原有的界面挡住(防止敏感信息被截屏，进而泄露出去)
+    //2.如果当前设置了PIN码，从后台返回时需要先输入PIN码（防止被其他人看到）
     if ([LXHWallet hasPIN])
         [self presentValidatePINViewController];
     else
