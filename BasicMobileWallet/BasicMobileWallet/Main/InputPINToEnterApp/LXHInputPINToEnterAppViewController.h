92381e60d937ca906044bb405189445861ad35e6
Lian <lianxianghui@hotmail.com>
2020-01-11 12:19:57 +0800

    加入输入PIN码View

---
+++ BasicMobileWallet/BasicMobileWallet/Main/InputPINToEnterApp/LXHInputPINToEnterAppViewController.h
@@ -0,0 +1,11 @@
+// LXHInputPINToEnterAppViewController.h
+// BasicWallet
+//
+//  Created by lianxianghui on 20-01-11
+//  Copyright © 2020年 lianxianghui. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@interface LXHInputPINToEnterAppViewController : UIViewController
+
+@end
---
+++ BasicMobileWallet/BasicMobileWallet/Main/InputPINToEnterApp/LXHInputPINToEnterAppViewController.m
@@ -0,0 +1,52 @@
+// LXHInputPINToEnterAppViewController.m
+// BasicWallet
+//
+//  Created by lianxianghui on 20-01-11
+//  Copyright © 2020年 lianxianghui. All rights reserved.
+
+#import "LXHInputPINToEnterAppViewController.h"
+#import "Masonry.h"
+#import "LXHInputPINToEnterAppView.h"
+
+#define UIColorFromRGBA(rgbaValue) \
+[UIColor colorWithRed:((rgbaValue & 0xFF000000) >> 24)/255.0 \
+        green:((rgbaValue & 0x00FF0000) >>  16)/255.0 \
+        blue:((rgbaValue & 0x0000FF00) >>  8)/255.0 \
+        alpha:(rgbaValue & 0x000000FF)/255.0]
+    
+@interface LXHInputPINToEnterAppViewController()
+@property (nonatomic) LXHInputPINToEnterAppView *contentView;
+
+@end
+
+@implementation LXHInputPINToEnterAppViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColorFromRGBA(0xFAFAFAFF);
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.contentView = [[LXHInputPINToEnterAppView alloc] init];
+    [self.view addSubview:self.contentView];
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_topLayoutGuideBottom);
+        make.left.right.equalTo(self.view);
+        make.bottom.equalTo(self.mas_bottomLayoutGuideTop);
+    }];
+    UISwipeGestureRecognizer *swipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeView:)];
+    [self.view addGestureRecognizer:swipeRecognizer];
+    [self addActions];
+    [self setDelegates];
+}
+
+- (void)swipeView:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)addActions {
+}
+
+- (void)setDelegates {
+}
+
+@end
