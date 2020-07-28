b7ef2b3adcc966a3ee3a613b9c4fb1a7308749be
Lian <lianxianghui@hotmail.com>
2020-07-28 14:32:12 +0800

    注释

--- BasicMobileWallet/BasicMobileWallet/Init/LXHInitFlow.h
+++ BasicMobileWallet/BasicMobileWallet/Init/LXHInitFlow.h
@@ -11,12 +11,18 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+
+/**
+ 从“初始化钱包”页面，可以开始创建新钱包、也可以恢复已有钱包。接下来的流程从开始到结束会经过多个页面。
+ 接下来的流程（从SelectMnemonicWordLength页开始）所经过的页面有的是完全不一样，而有的是页面功能基本一样，只是跳转逻辑不一样（比如"助记词数量"页点击列表项接下来会进入不同的页面)。
+ 后来又加入了重置PIN码的功能，也会从SelectMnemonicWordLength页开始走输入助记词的流程
+ 下面这个类 把这些页面跳转逻辑不一样的地方抽象成统一的接口，用子类来具体实现。
+ */
 @interface LXHInitFlow : NSObject
 
 @property (nonatomic) NSUInteger mnemonicWordsLength;
 @property (nonatomic) NSArray *mnemonicWords;
 @property (nonatomic) NSString *mnemonicPassphrase;
-//@property (nonatomic) LXHBitcoinNetworkType networkType;//目前未使用
 
 + (LXHInitFlow *)currentFlow;
 + (void)startCreatingNewWalletFlow;
