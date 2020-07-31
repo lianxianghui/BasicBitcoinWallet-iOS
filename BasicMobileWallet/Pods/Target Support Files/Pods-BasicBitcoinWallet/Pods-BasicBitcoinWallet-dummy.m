c582f3986eb5cad25278625b795e7fe5ec5c2025
Lian <lianxianghui@hotmail.com>
2020-07-31 15:12:49 +0800

    重命名目录

--- BasicMobileWallet/Pods/Target Support Files/Pods-BasicBitcoinWallet/Pods-BasicBitcoinWallet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BasicBitcoinWallet : NSObject
-@end
-@implementation PodsDummy_Pods_BasicBitcoinWallet
-@end
--- BasicMobileWallet/Pods/Target Support Files/Pods-BasicBitcoinWalletTests/Pods-BasicBitcoinWalletTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BasicBitcoinWalletTests : NSObject
-@end
-@implementation PodsDummy_Pods_BasicBitcoinWalletTests
-@end
